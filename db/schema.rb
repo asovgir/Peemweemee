ddb4627aab4eaf86c41d7423675adb3318435e75
root <andre.sovgir@gmail.com>
2019-08-29 21:53:41 -0700

    avatar + bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190827042532) do
+ActiveRecord::Schema.define(version: 20190830043857) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
 end
