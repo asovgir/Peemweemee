d76f9f34856ab18e65e1458c2a5c9ede02cc67aa
root <andre.sovgir@gmail.com>
2019-09-03 20:21:20 -0700

    added tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
+    @user2 = users(:two)
   end
 
   test "should get index" do
@@ -15,9 +16,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create user" do
+  test "should create user when image present" do
     assert_difference('User.count') do
       post users_url, params: { user: { username: @user.username } }
+    end
+
+    assert_redirected_to user_url(User.last)
+  end
+
+  test "should create user when image not present" do
+    assert_difference('User.count') do
+      post users_url, params: { user: { username: @user2.username } }
     end
 
     assert_redirected_to user_url(User.last)
